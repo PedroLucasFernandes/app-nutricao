2a6761101f331b3768a8118ebbd3151b280d19a5
Pedro Lucas Fernandes <plff201100@gmail.com>
2023-10-30 15:37:41 -0300

    fix: overflow bug in some phones

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,161 +6,164 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0XFFC7E5A1),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Alimentos:",
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Color(0XFF355211),
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Icon(
-                      Icons.search,
-                      color: Color(0XFF355211),  
-                    ),
-                  ],
-                ),
-                SizedBox(height: 8,),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color(0XFFE4FDC5),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Row(
+    return Scaffold(
+      backgroundColor: Color(0XFFC7E5A1),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          width: 124,
-                          height: 151,
+                      Text(
+                        "Alimentos:",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Color(0XFF355211),
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      Container(
-                        height: 151,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                            "Nome: Hambúrguer",
-                            ),
-                            Text(
-                              "Categoria: Carboidrato",
-                            ),
-                            Text(
-                              "Refeição: Janta",
-                            ),
-                          ],
-                        ),
+                      Icon(
+                        Icons.search,
+                        color: Color(0XFF355211),  
                       ),
                     ],
                   ),
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Cardápios:",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0XFF355211),
-                      ),
+                  SizedBox(height: 8,),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0XFFE4FDC5),
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                    Icon(
-                      Icons.search,
-                      color: Color(0XFF355211),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 8,),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color(0XFFE4FDC5),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    child: Row(
                       children: [
-                        Text(
-                          "Café:",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0XFF355211),
+                        Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            width: 124,
+                            height: 151,
                           ),
                         ),
                         Container(
-                          width: double.infinity,
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: Color(0XFFC7E5A1),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        SizedBox(height: 12,),
-                        Text(
-                          "Almoço:",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0XFF355211),
-                          ),  
-                        ),
-                        Container(
-                          width: double.infinity,
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: Color(0XFFC7E5A1),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        SizedBox(height: 12,),
-                        Text(
-                          "Jantar:",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0XFF355211),
-                          ),
-                        ),
-                        Container(
-                          width: double.infinity,
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: Color(0XFFC7E5A1),
-                            borderRadius: BorderRadius.circular(8),
+                          height: 151,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                              "Nome: Hambúrguer",
+                              ),
+                              Text(
+                                "Categoria: Carboidrato",
+                              ),
+                              Text(
+                                "Refeição: Janta",
+                              ),
+                            ],
                           ),
                         ),
                       ],
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+              SizedBox(height: 20,),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Cardápios:",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0XFF355211),
+                        ),
+                      ),
+                      Icon(
+                        Icons.search,
+                        color: Color(0XFF355211),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8,),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0XFFE4FDC5),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Café:",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0XFF355211),
+                            ),
+                          ),
+                          Container(
+                            width: double.infinity,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Color(0XFFC7E5A1),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          SizedBox(height: 12,),
+                          Text(
+                            "Almoço:",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0XFF355211),
+                            ),  
+                          ),
+                          Container(
+                            width: double.infinity,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Color(0XFFC7E5A1),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          SizedBox(height: 12,),
+                          Text(
+                            "Jantar:",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0XFF355211),
+                            ),
+                          ),
+                          Container(
+                            width: double.infinity,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Color(0XFFC7E5A1),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );  
