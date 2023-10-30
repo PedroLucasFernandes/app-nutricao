a2ef2d24d431ffd26fde5cb18717e0d5a1dce24a
Pedro Lucas Fernandes <plff201100@gmail.com>
2023-10-30 15:34:41 -0300

    fix: overflow bug

--- lib/pages/new_menu.dart
+++ lib/pages/new_menu.dart
@@ -5,124 +5,126 @@ class NovoCardapio extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0XFFC7E5A1),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Crie seu Cardápio",
-              style: TextStyle(
-                fontSize: 20,
-                color: Color(0XFF355211),
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 12,),
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0XFFE4FDC5),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Café:",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0XFF355211),
-                      ),
-                    ),
-                    Container(
-                      width: double.infinity,
-                      height: 110,
-                      decoration: BoxDecoration(
-                        color: Color(0XFFC7E5A1),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          labelText: "Digite aqui...",
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 12,),
-                    Text(
-                      "Almoço:",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0XFF355211),
-                      ),  
-                    ),
-                    Container(
-                      width: double.infinity,
-                      height: 110,
-                      decoration: BoxDecoration(
-                        color: Color(0XFFC7E5A1),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          labelText: "Digite aqui...",
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 12,),
-                    Text(
-                      "Jantar:",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0XFF355211),
-                      ),
-                    ),
-                    Container(
-                      width: double.infinity,
-                      height: 110,
-                      decoration: BoxDecoration(
-                        color: Color(0XFFC7E5A1),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          labelText: "Digite aqui...",
-                        ),
-                      ),
-                    ),
-                  ],
+    return Scaffold(
+      backgroundColor: Color(0XFFC7E5A1),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Crie seu Cardápio",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color(0XFF355211),
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-            SizedBox(height: 12,),
-            ElevatedButton(
-              onPressed: () {
-
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(Color(0XFF355211)),
-                shape: MaterialStatePropertyAll(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
+              SizedBox(height: 12,),
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0XFFE4FDC5),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Café:",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0XFF355211),
+                        ),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 110,
+                        decoration: BoxDecoration(
+                          color: Color(0XFFC7E5A1),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Digite aqui...",
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 12,),
+                      Text(
+                        "Almoço:",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0XFF355211),
+                        ),  
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 110,
+                        decoration: BoxDecoration(
+                          color: Color(0XFFC7E5A1),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Digite aqui...",
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 12,),
+                      Text(
+                        "Jantar:",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0XFF355211),
+                        ),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 110,
+                        decoration: BoxDecoration(
+                          color: Color(0XFFC7E5A1),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Digite aqui...",
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-              child: Text(
-                "Cadastrar",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
+              SizedBox(height: 12,),
+              ElevatedButton(
+                onPressed: () {
+
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Color(0XFF355211)),
+                  shape: MaterialStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+                child: Text(
+                  "Cadastrar",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
