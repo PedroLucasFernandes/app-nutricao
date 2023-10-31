f4c2784f9339460fbffc0f3f3c1fdd0938f8b994
Pedro Lucas Fernandes <plff201100@gmail.com>
2023-10-31 20:23:00 -0300

    fix: add github and linkedin buttons

--- lib/pages/credits_page.dart
+++ lib/pages/credits_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CreditsPage extends StatelessWidget {
-  const CreditsPage({super.key});
+  CreditsPage({super.key});
+
+  final Uri githubUrl = Uri.parse('https://github.com/PedroLucasFernandes');
+  final Uri linkedinUrl = Uri.parse('https://www.linkedin.com/in/pedro-fernandes-ferreira-14b1561a1/');
 
   @override
   Widget build(BuildContext context) {
@@ -56,6 +60,93 @@ class CreditsPage extends StatelessWidget {
                 fontSize: 20,
                 color: Color(0XFF355211),
                 fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 70.0,
+                vertical: 15.0
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0XFF355211), width: 3.0),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: ElevatedButton(
+                  onPressed: () async {           
+                    try {
+                      await launchUrl(githubUrl);
+                    } catch(e) {
+                      
+                    }
+                  },
+                  style: ButtonStyle(
+                    elevation: MaterialStatePropertyAll(0),
+                    backgroundColor: MaterialStatePropertyAll(Colors.transparent)
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        "assets/github.png",
+                        height: 40,
+                        width: 40,
+                      ),
+                      SizedBox(width: 25,),
+                      Text(
+                        "My GitHub",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 70.0,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0XFF355211), width: 3.0),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: ElevatedButton(
+                  onPressed: () async {  
+                    try {
+                      await launchUrl(linkedinUrl);
+                    } catch(e) {
+                      
+                    }
+                  },
+                  style: ButtonStyle(
+                    elevation: MaterialStatePropertyAll(0),
+                    backgroundColor: MaterialStatePropertyAll(Colors.transparent)
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        "assets/linkedin.png",
+                        height: 40,
+                        width: 40,
+                      ),
+                      SizedBox(width: 25,),
+                      Text(
+                        "My LinkedIn",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0XFF047CB4),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
