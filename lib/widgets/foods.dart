21f02102d22a25b806e610f878e69cacad0a8b56
Pedro Lucas Fernandes <plff201100@gmail.com>
2023-11-02 13:53:01 -0300

    refactor: add food class

---
+++ lib/widgets/foods.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class Foods extends StatelessWidget {
+
+  final String name;
+  final Image? image;
+  final String type;
+  final String meal;
+  const Foods({
+      super.key,
+      this.image, 
+      required this.name, 
+      required this.type, 
+      required this.meal, 
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Color(0XFF355211),
+          width: 2.0,
+        ),
+        borderRadius: BorderRadius.circular(15)
+      ),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              width: 124,
+              height: 125,
+            ),
+          ),
+          Container(
+            height: 151,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                "Nome: $name",
+                ),
+                Text(
+                  "Categoria: $type",
+                ),
+                Text(
+                  "Refeição: $meal",
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        Icons.create,
+                        color: Colors.black,
+                        size: 24.0,
+                      ),
+                      onPressed: () {
+      
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.delete,
+                        color: Colors.red,
+                        size: 24.0,
+                      ),
+                      onPressed: () {
+                        
+                      },
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
